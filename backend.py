c4d0cb933ead7902ddceac04eb6a7b1b4a9d2e6e
Srikar Chitturi <64421927+SrikarC6@users.noreply.github.com>
2026-03-23 00:57:16 -0400

    refactor(backend): Converted link exporting logic into a function

--- backend.py
+++ backend.py
@@ -1,41 +1,40 @@
 import requests
 
-album_link = "https://itunes.apple.com/search?term=ALBUMNAME&entity=album&attribute=albumTerm&limit=25"
-song_link = "https://itunes.apple.com/search?term=SONGNAME&entity=song&attribute=songTerm&limit=25"
-artist_link = "https://itunes.apple.com/search?term=ARTISTNAME&entity=album&attribute=artistTerm&limit=25"
+def search_artwork(search_type, name):
+    album_link = "https://itunes.apple.com/search?term=ALBUMNAME&entity=album&attribute=albumTerm&limit=25"
+    song_link = "https://itunes.apple.com/search?term=SONGNAME&entity=song&attribute=songTerm&limit=25"
+    artist_link = "https://itunes.apple.com/search?term=ARTISTNAME&entity=album&attribute=artistTerm&limit=25"
+    artwork_links = []
+    name = name.replace(' ', '+')
 
-search_type = input("Enter where you want to download from (album/song/artist): ").lower()
-artwork_links = []
-
-if search_type == 'album':
-    album_name = input("Enter the album to download: ").replace(' ', '+')
-    album_link = album_link.replace("ALBUMNAME", album_name)
-    album_requests_dict = requests.get(album_link).json()
-    album_results = album_requests_dict['results']
+    if search_type == 'album':
+        album_link = album_link.replace("ALBUMNAME", name)
+        album_requests_dict = requests.get(album_link).json()
+        album_results = album_requests_dict['results']
     
-    for album in album_results:
-        album['artworkUrl100'] = album['artworkUrl100'].replace('100x100bb', '10000x10000bb')
-        artwork_links.append(album['artworkUrl100'])
+        for album in album_results:
+            album['artworkUrl100'] = album['artworkUrl100'].replace('100x100bb', '10000x10000bb')
+            artwork_links.append(album['artworkUrl100'])
 
-elif search_type == 'song':
-    song_name = input("Enter the song to download: ").replace(' ', '+')
-    song_link = song_link.replace("SONGNAME", song_name)
-    song_requests_dict = requests.get(song_link).json()
-    song_results = song_requests_dict['results']
+    elif search_type == 'song':
+        song_link = song_link.replace("SONGNAME", name)
+        song_requests_dict = requests.get(song_link).json()
+        song_results = song_requests_dict['results']
     
-    for song in song_results:
-        song['artworkUrl100'] = song['artworkUrl100'].replace('100x100bb', '10000x10000bb')
-        artwork_links.append(song['artworkUrl100'])
+        for song in song_results:
+            song['artworkUrl100'] = song['artworkUrl100'].replace('100x100bb', '10000x10000bb')
+            artwork_links.append(song['artworkUrl100'])
 
-elif search_type == 'artist':
-    artist_name = input("Enter the artist's albums to download: ").replace(' ', '+')
-    artist_link = artist_link.replace("ARTISTNAME", artist_name)
-    artist_requests_dict = requests.get(artist_link).json()
-    artist_results = artist_requests_dict['results']
+    elif search_type == 'artist':
+        artist_link = artist_link.replace("ARTISTNAME", name)
+        artist_requests_dict = requests.get(artist_link).json()
+        artist_results = artist_requests_dict['results']
     
-    for album in artist_results:
-        album['artworkUrl100'] = album['artworkUrl100'].replace('100x100bb', '10000x10000bb')
-        artwork_links.append(album['artworkUrl100'])
+        for album in artist_results:
+            album['artworkUrl100'] = album['artworkUrl100'].replace('100x100bb', '10000x10000bb')
+            artwork_links.append(album['artworkUrl100'])
 
-else:
-    raise ValueError("Invalid search type. Please enter album, song, or artist.")
+    else:
+        raise ValueError("Invalid search type. Please enter album, song, or artist.")
+        
+    return artwork_links
